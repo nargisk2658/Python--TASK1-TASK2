6de8e83fb3c36cae6a204297004a33d076d15093
nargisk2658 <nargisk7170@gmail.com>
2025-11-19 22:53:11 +0530

    Update T2.py

--- Assignment2/T2.py
+++ Assignment2/T2.py
@@ -1,5 +1,11 @@
+'''Write a Python program that:
+1.   Uses a for loop to iterate over numbers from 1 to 50.
+2.   Calculates the sum of all integers in this range.
+3.   Displays the final sum.'''
+
 sum=0
 for num in range(1,51):
     sum+=num
 print(f"The sum of integers from 1 to 50 is: {sum}")
+
     
