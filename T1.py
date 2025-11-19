82acc220dfd5457421107206335a0713ebf1f6e8
nargisk2658 <nargisk7170@gmail.com>
2025-11-19 22:35:10 +0530

    Add files via upload

---
+++ T1.py
@@ -0,0 +1,5 @@
+num = int(input("Enter an integar number: "))
+if num % 2 :
+    print (num, "is an odd number.")
+else :
+    print(num, "is an even number.")
---
+++ T2.py
@@ -0,0 +1,5 @@
+sum=0
+for num in range(1,51):
+    sum+=num
+print(f"The sum of integers from 1 to 50 is: {sum}")
+    
