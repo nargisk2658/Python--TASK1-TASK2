c1f1b14fe9a8cf91073293ae92650cd9cbe45fea
nargisk2658 <nargisk7170@gmail.com>
2025-11-19 22:50:22 +0530

    Add files via upload

---
+++ Assignment1/task2.py
@@ -0,0 +1,8 @@
+''' Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+'''
+a = input("Enter the first name: ")
+b = input("Enter the second name: ")
+print("Hello", a+b, "! Welcome to Python program")
